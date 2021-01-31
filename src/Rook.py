bf28b65041aa658d2a73add3f79d90260e7ec5fc
sankalp-sangle <sanku.sangle@gmail.com>
2021-01-31 13:20:59 +0530

    Added rook movement. Todo: cannot move if pinned

--- src/Rook.py
+++ src/Rook.py
@@ -8,3 +8,23 @@ class Rook:
         self.height = 78
         self.width = 96
         self.isWhite = isWhite
+
+    def validate(self, newX, newY, board):
+        # If invalid movement return False
+        if newX != self.x and newY != self.y:
+            return False
+
+        add_to_x = 0 if self.x == newX else (1 if newX > self.x else -1)
+        add_to_y = 0 if self.y == newY else (1 if newY > self.y else -1)
+
+        # If any obstacles in the way, return False
+        x, y = self.x + add_to_x, self.y + add_to_y
+        for i in range(max(abs(newX - self.x), abs(newY - self.y)) - 1):
+            if board.squares[x][y].isOccupied:
+                return False
+            x += add_to_x
+            y += add_to_y
+
+        # If destination is not occupied, return True.
+        # If destination is occupied, but occupied by a different colour, return True. Else return False.
+        return not board.squares[newX][newY].isOccupied or (self.isWhite != board.squares[newX][newY].piece.isWhite)
